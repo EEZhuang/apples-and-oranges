0891d8e7d04f197c7210f1e7c8fd5d5ee5ae1612
EEZhuang <43657787+EEZhuang@users.noreply.github.com>
2020-07-15 19:31:41 -0700

    Fridge screen (#15)
    * Fixed spacing of food in fridge
    
    Co-authored-by: Emily-Zhuang <Emily.Zhuang@ibm.com>
    Co-authored-by: Justin Chen <justin.chen@columbia.edu>

--- starter-kit/mobile-app/src/screens/fridge.js
+++ starter-kit/mobile-app/src/screens/fridge.js
@@ -2,6 +2,121 @@ import React, { Component } from 'react';
 import { StyleSheet, View, Image, Text, TouchableOpacity, TouchableHighlight, Button, Linking, Alert } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 
+class Fridge extends Component {
+  constructor (props) {
+    super(props);
+    this.wrapInFridge = this.wrapInFridge.bind(this);
+    this.getShelves = this.getShelves.bind(this);
+    this.getAllFoodItems = this.getAllFoodItems.bind(this);
+
+    this.state = {
+      allFoodItems: []
+    };
+  }
+
+  componentDidMount() {
+    this.getAllFoodItems();
+    this._navListener = this.props.navigation.addListener('focus', () => {
+      this.getAllFoodItems();
+     });
+  }
+
+  add() {
+    this.props.navigation.navigate('Add')
+  }
+
+  wrapInFridge() {
+      const Shelves = () => this.getShelves();
+
+      return (
+        <View style={styles.center}>
+          <ScrollView style={styles.scroll}>
+          <Text style={styles.title}>Food in your fridge</Text>
+            <Text style={styles.subtitle}>You have {this.state.allFoodItems.length} items in your fridge!</Text>
+            <View style={styles.buttonGroup}>
+              <TouchableOpacity onPress={() => this.add()}>
+                <Text style={styles.button}>Went grocery shopping? Add more!</Text>
+              </TouchableOpacity>
+            </View>
+            <Shelves />
+          </ScrollView>
+        </View>
+
+      );
+  }
+
+  getShelves() {
+    let allFoodItems = this.state.allFoodItems;
+
+    // Column
+    let shelves = [];
+    // Row
+    let shelf = [];
+    let numItems = 0;
+
+    // Traverse items in order
+    let i = 0;
+    for (i; i < allFoodItems.length; i++) {
+      let food = allFoodItems[i];
+      let foodItem =
+        <TouchableHighlight
+          onPress={() => this.props.navigation.navigate('Details', { json: food })}
+          underlayColor='#dcdcdc'
+        >
+          <View>
+            <Text style={{fontSize:50, alignItems:'center', marginBottom:10}}>{food.emoji}</Text>
+          </View>
+         </TouchableHighlight>
+      shelf.push(foodItem);
+      numItems++;
+
+      // If row is filled or last row
+      if (numItems % 4 === 0 || numItems === allFoodItems.length) {
+        shelves.push(
+          <View style={styles.center}>
+            <View style={{width: 375, height: 20}}/>
+            <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-around'}}>
+              <View style={{width: 30, height: 65}}></View>
+              <View style={{width: 60, height: 65}}>{shelf[0]}</View>
+              <View style={{width: 30, height: 65}}></View>
+              <View style={{width: 60, height: 65}}>{shelf[1]}</View>
+              <View style={{width: 30, height: 65}}></View>
+              <View style={{width: 60, height: 65}}>{shelf[2]}</View>
+              <View style={{width: 30, height: 65}}></View>
+              <View style={{width: 60, height: 65}}>{shelf[3]}</View>
+            </View>
+            <View style={{width: 375, height: 10, backgroundColor: 'powderblue'}}/>
+          </View>
+        );
+
+        // Reset to empty shelf
+        shelf = [];
+      }
+    };
+
+    return shelves;
+  }
+
+  getAllFoodItems() {
+      // get all food items
+    fetch('http://localhost:3000/food', {method: 'GET'})
+    .then(res => res.json())
+    .then(json => this.setState({ allFoodItems: json }))
+    .catch((error) => {
+      Alert.alert('Error', 'There was a problem retrieving food items');
+      console.error(error);
+    });
+  }
+
+  render = () => {
+    const Fridge = () => this.wrapInFridge();
+
+    return (
+      <Fridge />
+    );
+  };
+}
+
 const styles = StyleSheet.create({
   center: {
     flex: 1,
@@ -68,117 +183,5 @@ const styles = StyleSheet.create({
     marginTop: 15
   }
 });
-
-class Fridge extends Component {
-  constructor (props) {
-    super(props);
-    this.wrapInFridge = this.wrapInFridge.bind(this);
-    this.getShelves = this.getShelves.bind(this);
-    this.getAllFoodItems = this.getAllFoodItems.bind(this);
-
-    this.state = {
-      allFoodItems: []
-    };
-  }
-
-  componentDidMount() {
-    this.getAllFoodItems();
-    this._navListener = this.props.navigation.addListener('focus', () => {
-      this.getAllFoodItems();
-     });
-  }
-
-  add() {
-    this.props.navigation.navigate('Add')
-  }
-
-  wrapInFridge() {
-      const Shelves = () => this.getShelves();
-
-      return (
-        <View style={styles.center}>
-          <ScrollView style={styles.scroll}>
-          <Text style={styles.title}>Food in your fridge</Text>
-            <Text style={styles.subtitle}>You have {this.state.allFoodItems.length} items in your fridge!</Text>
-            <View style={styles.buttonGroup}>
-              <TouchableOpacity onPress={() => this.add()}>
-                <Text style={styles.button}>Went grocery shopping? Add more!</Text>
-              </TouchableOpacity>
-            </View>
-            <Shelves />
-          </ScrollView>
-        </View>
-
-      );
-  }
-
-  getShelves() {
-    let allFoodItems = this.state.allFoodItems;
-
-    // Column
-    let shelves = [];
-    // Row
-    let shelf = [];
-    let numItems = 0;
-
-    // Traverse items in order
-    let i = 0;
-    for (i; i < allFoodItems.length; i++) {
-      let food = allFoodItems[i];
-      let foodItem =
-        <TouchableHighlight
-          onPress={() => this.props.navigation.navigate('Details', { json: food })}
-          underlayColor='#dcdcdc'
-        >
-          <View>
-            <Text style={{fontSize:50}}>o</Text>
-            <Text style={{fontSize:50}}>{food.emoji}</Text>
-          </View>
-         </TouchableHighlight>
-      shelf.push(foodItem);
-      numItems++;
-
-      // Row is filled or last row
-      if (numItems % 4 === 0 || numItems === allFoodItems.length) {
-        shelves.push(
-          <View style={styles.center}>
-            <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-around'}}>
-              {shelf}
-            </View>
-            <View style={{width: 370, height: 10, backgroundColor: 'powderblue'}}/>
-          </View>
-        );
-
-        // Reset to empty shelf
-        shelf = [];
-      }
-    };
-
-    return shelves;
-  }
-
-  getAllFoodItems() {
-      // get all food items
-      /*
-      */
-    fetch('http://localhost:3000/food', {method: 'GET'})
-    .then(res => res.json())
-    //.then(json => console.log(json))
-    //.then(json => allFoodItems = json)
-    .then(json => this.setState({ allFoodItems: json }))
-    .catch((error) => {
-      Alert.alert('Error', 'There was a problem retrieving food items');
-      console.error(error);
-    });
-  }
-
-  render = () => {
-    const Fridge = () => this.wrapInFridge();
-
-    return (
-      <Fridge />
-    );
-  };
-}
 
 export default Fridge;
